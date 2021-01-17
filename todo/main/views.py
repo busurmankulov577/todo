0d2a4c97209a0cb129eba0c0794a19488dc70fea
Damirbek <busurmankulov577@gmail.com>
2021-01-17 17:11:39 +0600

    set media and static roots

--- todo/main/views.py
+++ todo/main/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render, HttpResponse
 
 
 def homepage(requests):
-    return HttpResponse("Hello beatifull world")
+    return render(requests, "index.html")
 
 def go(request):
     return HttpResponse("THIS is MY first Page")
 
 def test(requests):
     return render(requests, "test.html")
+
+def second(request):
+    return HttpResponse("test 2 page")
 
 # Create your views here.
